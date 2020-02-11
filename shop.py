0291e1428b66d69d7c33d594f2b8dd0c3798d2c0
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-02-11 12:05:27 +0200

    SYTYCD-Python

--- shop.py
+++ shop.py
@@ -1,4 +1,5 @@
 ####################### DO NOT MODIFY THIS CODE ########################
+import string
 menu = {
     "original cupcake": 2,
     "signature cupcake": 2.750,
@@ -21,7 +22,7 @@ def print_menu():
     Print the items in the menu dictionary.
     """
     print("Our menu:")
-    for item in cupcake_menu:
+    for item in menu:
         print("- \"%s\" (KD %s)" % (item, menu[item]))
 
 
@@ -33,7 +34,7 @@ def print_originals():
     """
     print("Our original flavor cupcakes (KD %s each):" % original_price)
     for item in original_flavors:
-    print("- \"%s\"" % item)
+        print("- \"%s\"" % item)
 
 
 def print_signatures():
@@ -65,7 +66,7 @@ def get_order():
     """
     order_list = []
     order = input("What's your order? (Enter the exact spelling of the item you want. Type 'Exit' to end your order.)\n")
-    while order.lowercase() != "exit":
+    while order.lower() != "exit":
         if is_valid_order(order):
             order_list.append(order)
         order = input()
